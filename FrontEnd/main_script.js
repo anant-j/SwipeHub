c36089854d1a51c616a63e0e4d40132b99150311
Anant Jain <anant.j2409@gmail.com>
2021-05-30 13:30:23 -0400

    Added fields to CreateSession

--- FrontEnd/main_script.js
+++ FrontEnd/main_script.js
@@ -44,12 +44,12 @@ function joinSession() {
   xhr.send(null);
 }
 
-function showLoader(){
+function showLoader() {
   document.getElementById("loading").style.display = "block";
   document.getElementById("outer_container").style.display = "none";
 }
 
-function hideLoader(){
+function hideLoader() {
   document.getElementById("loading").style.display = "none";
   document.getElementById("outer_container").style.display = "flex";
 }
@@ -59,7 +59,7 @@ var btns = header.getElementsByClassName("genreButton");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     // var current = document.getElementsByClassName("active");
-    if (this.className.includes("active")) {
+    if (this.className.includes("create_active")) {
       this.className = this.className.replace(" create_active", "");
     } else {
       this.className += " create_active";
@@ -70,16 +70,23 @@ for (var i = 0; i < btns.length; i++) {
 
 function createSession() {
   console.log("creating session");
-  const input_id = document.getElementById("sessionId").value;
-  showLoader();
+  // showLoader();
   var xhr = new XMLHttpRequest();
   const username = document.getElementById("email").value;
   var header = document.getElementById("multiButtonGroup");
-  var btns = header.getElementsByClassName("active");
+  var btns = header.getElementsByClassName("create_active");
+  var mix = true;
+  if (document.getElementById("btnradio1").checked) {
+    mix = false;
+  }
   var categories = "";
   for (var i = 0; i < btns.length; i++) {
-    // console.log(btns[i].id);
-    categories += btns[i].id + `|`;
+    if (mix) {
+      categories += btns[i].id + `|`;
+    }
+    else {
+      categories += btns[i].id + `,`;
+    }
   }
   categories = categories.substring(0, categories.length - 1);
   const languages = document.getElementById("language").value;
@@ -117,3 +124,63 @@ function openSessionPage() {
     alert("Please create or join a session first");
   }
 }
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  let isInitialVisible = document.getElementById("initialButtons").style.display != "none";
+  if (!isInitialVisible) {
+    return;
+  }
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+
+  currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function () {
+    cw[i].className = 'letter out';
+  }, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function () {
+    nw[i].className = 'letter in';
+  }, 340 + (i * 80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
