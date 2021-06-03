2b5dfe473ece720153b7bdf145aaa4c64199875d
Anant Jain <anant.j2409@gmail.com>
2021-06-02 22:42:24 -0400

    Major file order restructure, implemented alerts, minor UI changes

---
+++ FrontEnd/main.js
@@ -0,0 +1,73 @@
+'use strict';
+
+// const baseUrl = 'http://localhost:5001/tinder-netflix/us-central1';
+const baseUrl = 'https://us-central1-tinder-netflix.cloudfunctions.net';
+const storage = window.localStorage;
+
+
+function createAlert(content, type) {
+  type = type.toLowerCase();
+  let svgStr='';
+  switch (type) {
+    case 'danger':
+      svgStr = '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>';
+      break;
+    case 'success':
+      svgStr = '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>';
+      break;
+    case 'info':
+      svgStr = '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>';
+      break;
+    case 'warning':
+      svgStr = '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>';
+      break;
+    default:
+      console.log('Error while raising alert');
+      return;
+              // code block
+  }
+  const date = new Date();
+  const now = date.getTime().toString();
+  const div = document.createElement('div');
+  div.id = now;
+  div.innerHTML = `${svgStr}<div>${content}</div>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`;
+  div.className = `alert alert-${type} d-flex align-items-center alert-dismissible fade show mt-3`;
+  div.role = 'alert';
+  document.getElementById('alertContainer').appendChild(div);
+  $(`#${now}`).fadeTo(3000, 100).slideUp(500, function() {
+    $(`#${now}`).slideUp(500);
+    document.getElementById(`${now}`).remove();
+  });
+}
+
+function showLoader() {
+  document.getElementById('loading').style.display = 'block';
+  document.getElementById('outer_container').style.display = 'none';
+}
+
+function hideLoader() {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('outer_container').style.display = 'flex';
+}
+
+function copyToClipboard(item) {
+  let data = '';
+  let text = '';
+  switch (item) {
+    case 'userId':
+      data = userId;
+      text = 'User Id';
+      break;
+    case 'sessionId':
+      data = sessionId;
+      text = 'Session Id';
+      break;
+    default:
+      createAlert(`Alert occurred while copying to clipboard`, 'danger');
+          // code block
+  }
+  navigator.clipboard.writeText(data);
+  createAlert(`${text} copied to clipboard`, 'success');
+}
+
---
+++ FrontEnd/session.js
@@ -0,0 +1,318 @@
+const tinderContainer = document.querySelector('.tinder');
+const allCards = document.querySelectorAll('.tinder--card');
+const nope = document.getElementById('nope');
+const love = document.getElementById('love');
+const sessionId = localStorage.getItem('Shwiper_sessionId');
+const userId = localStorage.getItem('Shwiper_userId');
+const globalHammerTime = {};
+const globalLikeBuffer = new Set();
+let totalSwipes = 0;
+let numMatches = 0;
+
+if (sessionId === null || userId === null) {
+  window.location.href = './index.html';
+}
+
+document.getElementById('userIdPlaceholder').innerHTML = `User ID: <b>${userId}</b>`;
+document.getElementById('sessionIdPlaceholder').innerHTML = `Session ID: <b>${sessionId}</b>`;
+
+function initCards() {
+  const newCards = document.querySelectorAll('.tinder--card:not(.removed)');
+
+  newCards.forEach(function(card, index) {
+    card.style.zIndex = allCards.length - index;
+    card.style.transform =
+      'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+    card.style.opacity = (10 - index) / 10;
+  });
+
+  tinderContainer.classList.add('loaded');
+}
+
+initCards();
+
+function hammertimeEach(el) {
+  const hammertime = new Hammer(el);
+
+  hammertime.on('pan', function(event) {
+    el.classList.add('moving');
+  });
+
+  hammertime.on('pan', function(event) {
+    if (event.deltaX === 0) return;
+    if (event.center.x === 0 && event.center.y === 0) return;
+
+    tinderContainer.classList.toggle('tinder_love', event.deltaX > 0);
+    tinderContainer.classList.toggle('tinder_nope', event.deltaX < 0);
+
+    const xMulti = event.deltaX * 0.03;
+    const yMulti = event.deltaY / 80;
+    const rotate = xMulti * yMulti;
+
+    event.target.style.transform =
+      'translate(' +
+      event.deltaX +
+      'px, ' +
+      event.deltaY +
+      'px) rotate(' +
+      rotate +
+      'deg)';
+  });
+
+  hammertime.on('panend', function(event) {
+    el.classList.remove('moving');
+    tinderContainer.classList.remove('tinder_love');
+    tinderContainer.classList.remove('tinder_nope');
+
+    const moveOutWidth = document.body.clientWidth;
+    const keep = Math.abs(event.deltaX) < 50 || Math.abs(event.velocityX) < 0.4;
+
+    event.target.classList.toggle('removed', !keep);
+
+    if (keep) {
+      event.target.style.transform = '';
+    } else {
+      const endX = Math.max(
+          Math.abs(event.velocityX) * moveOutWidth,
+          moveOutWidth,
+      );
+      const toX = event.deltaX > 0 ? endX : -endX;
+      const endY = Math.abs(event.velocityY) * moveOutWidth;
+      const toY = event.deltaY > 0 ? endY : -endY;
+      const xMulti = event.deltaX * 0.03;
+      const yMulti = event.deltaY / 80;
+      const rotate = xMulti * yMulti;
+
+      if (event.deltaX > 0) {
+        rightSwipe();
+      }
+      if (event.deltaX < 0) {
+        leftSwipe();
+      }
+      event.target.style.transform =
+        'translate(' +
+        toX +
+        'px, ' +
+        (toY + event.deltaY) +
+        'px) rotate(' +
+        rotate +
+        'deg)';
+      initCards();
+    }
+  });
+}
+
+function createButtonListener(love) {
+  return function(event) {
+    const cards = document.querySelectorAll('.tinder--card:not(.removed)');
+    const moveOutWidth = document.body.clientWidth * 1.5;
+
+    if (!cards.length) return false;
+
+    const card = cards[0];
+
+    card.classList.add('removed');
+    if (love) {
+      card.style.transform =
+        'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+      tinderContainer.classList.toggle('tinder_love');
+      rightSwipe();
+      setTimeout(function() {
+        tinderContainer.classList.remove('tinder_love');
+      }, 1000);
+    } else {
+      card.style.transform =
+        'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+      tinderContainer.classList.toggle('tinder_nope');
+      leftSwipe();
+      setTimeout(function() {
+        tinderContainer.classList.remove('tinder_nope');
+      }, 1000);
+    }
+
+    initCards();
+
+    event.preventDefault();
+  };
+}
+
+const nopeListener = createButtonListener(false);
+const loveListener = createButtonListener(true);
+
+nope.addEventListener('click', nopeListener);
+love.addEventListener('click', loveListener);
+
+function rightSwipe() {
+  const cards = document.querySelectorAll('.tinder--card.removed');
+  const card = cards[cards.length - 1];
+  // console.log("right: " + card.id + " : " +document.getElementById(`text${card.id}`).innerText);
+  document.getElementById(`${card.id}`).className = '.tinder--card removed';
+  hammertimeFirstOnly();
+  globalLikeBuffer.add(card.id);
+  totalSwipes += 1;
+}
+function leftSwipe() {
+  const cards = document.querySelectorAll('.tinder--card.removed');
+  const card = cards[cards.length - 1];
+  // console.log("left: " + card.id + " : " +document.getElementById(`text${card.id}`).innerText);
+  // document.getElementById(`${card.id}`).style.display = "none";
+  document.getElementById(`${card.id}`).className = '.tinder--card removed';
+  hammertimeFirstOnly();
+  totalSwipes += 1;
+}
+
+function addMovieCard(imgurl, title, text, mediaId, release, adult) {
+  const div = document.createElement('div');
+  div.id = mediaId;
+  let adultResult='';
+  if (adult) {
+    adultResult = '🔞';
+  }
+  div.innerHTML = `<img id="img${mediaId}" src="${imgurl}"><p id="rightCard${mediaId}" class="rightCard">Released on:<br><b>${release}</b><br><br><span style="font-size:30px">${adultResult}</span></p>
+    <h3 id="text${mediaId}"><u>${title}</u></h3>
+    <p>${text}</p>`;
+  div.className = 'tinder--card';
+  div.onclick = function() {
+    if (document.getElementById(`img${mediaId}`).style.display != 'none') {
+      document.getElementById(`img${mediaId}`).style.display = 'none';
+      document.getElementById(`text${mediaId}`).style.paddingTop = '20px';
+      document.getElementById(`rightCard${mediaId}`).style.float = 'none';
+      document.getElementById(`rightCard${mediaId}`).style.marginRight = '0px';
+    } else {
+      document.getElementById(`img${mediaId}`).style.display = 'inline';
+      document.getElementById(`rightCard${mediaId}`).style.float = 'right';
+      document.getElementById(`text${mediaId}`).style.paddingTop = '10px';
+      document.getElementById(`rightCard${mediaId}`).style.marginRight = '30px';
+    }
+  };
+  document.getElementById('outerCardBody').appendChild(div);
+  initCards();
+  hammertimeFirstOnly();
+  // hammertimeEach(document.getElementById(mediaId));
+}
+
+function addLastCard() {
+  const div = document.createElement('div');
+  div.id = '-1';
+  div.innerHTML = `<h3 id="text-1"><u>Uh Oh</u></h3>
+    <p>Looks like we've run out of choices to show you for this session. <br>Swipe right to see more options or swipe left to quit the session. <br> If you are the session creator, the session will end.</p>`;
+  div.className = 'tinder--card';
+  document.getElementById('outerCardBody').appendChild(div);
+  initCards();
+  hammertimeFirstOnly();
+}
+
+function hammertimeFirstOnly() {
+  const newCards = document.querySelectorAll('.tinder--card:not(.removed)');
+  const removedCards = document.getElementsByClassName('.tinder--card removed');
+  if (!(newCards.length==0) && globalHammerTime[newCards[0].id] === undefined) {
+    hammertimeEach(newCards[0]);
+    globalHammerTime[newCards[0].id] = true;
+  }
+  if (newCards.length==0 && (removedCards[removedCards.length-1].id != -1)) {
+    addLastCard();
+  }
+}
+
+function joinSession() {
+  showLoader();
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+      'GET',
+      `${baseUrl}/joinSession?id=${sessionId}&user=${userId}`,
+      true,
+  );
+  xhr.onload = function() {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        const allData = JSON.parse(xhr.responseText);
+        const data = allData.movies;
+        if (allData.isCreator) {
+          document.getElementById('leaveSessionBtn').innerHTML = 'End Session';
+        }
+        for (const key in data) {
+          if (data.hasOwnProperty(key)) {
+            addMovieCard(
+                data[key]['poster'],
+                data[key]['title'],
+                data[key]['description'],
+                key,
+                data[key]['release_date'],
+                data[key]['adult'],
+            );
+          }
+        }
+        hideLoader();
+        poll();
+      } else {
+        // alert('Cannot load the session');
+        storage.removeItem('Shwiper_sessionId');
+        window.location.href = './index.html';
+      }
+    }
+  };
+  xhr.ontimeout = function(e) {
+    // alert('Cannot load the session');
+    storage.removeItem('Shwiper_sessionId');
+    window.location.href = './index.html';
+  };
+
+  xhr.send(null);
+}
+
+joinSession();
+
+
+function leaveSession() {
+  showLoader();
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+      'GET',
+      `${baseUrl}/leaveSession?id=${sessionId}&user=${userId}`,
+      true,
+  );
+  xhr.onload = function() {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+      }
+    }
+  };
+  hideLoader();
+  storage.removeItem('Shwiper_sessionId');
+  window.location.href = './index.html';
+
+  xhr.send(null);
+}
+
+function poll() {
+  const xhr = new XMLHttpRequest();
+  const sendBuffer = [];
+  globalLikeBuffer.forEach((v) => sendBuffer.push(v));
+  console.log(sendBuffer);
+  const params = `totalSwipes=${totalSwipes}&likedList=${sendBuffer}&sessionId=${sessionId}&userId=${userId}`;
+  // let params = `{userId: "jmhj"}`;
+  xhr.open(
+      'POST',
+      `${baseUrl}/polling`,
+      true,
+  );
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.onload = function() {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        const allData = JSON.parse(xhr.responseText);
+        const matchData = allData.match.length;
+        if (matchData>=1 && matchData!=numMatches) {
+          createAlert(`You've got: ${matchData} matches. <a href="./matches.html">Click Here to view them</a>`, 'success');
+          numMatches = matchData;
+          document.getElementById('matchTab').innerHTML=`Matches (${numMatches})`;
+        }
+      } else {
+        console.log('Polling failed');
+      }
+    }
+  };
+
+  xhr.send(params);
+  setTimeout(poll, 5000);
+}
