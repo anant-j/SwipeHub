fa17b7ed0260610e7ae248d4466c00dd310635aa
Anant Jain <anant.j2409@gmail.com>
2021-06-19 23:58:07 -0400

    Added vuex state management

--- swipehub/src/store/index.js
+++ swipehub/src/store/index.js
@@ -1,12 +1,20 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // state: {
+  // },
+  // mutations: {
+  // },
+  // actions: {
+  // },
+  // modules: {
+  // }
   state: {
+    count: 0
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    increment (state) {
+      state.count++
+    }
   }
 })
