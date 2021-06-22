0b5b648747d8e5aea8c43a5b1ccc5becc78e2074
Anant Jain <anant.j2409@gmail.com>
2021-06-22 10:43:00 -0400

    Adding Google Tracking

--- project/src/main.js
+++ project/src/main.js
@@ -7,6 +7,7 @@ import "./assets/alert.css"
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false
 
@@ -16,6 +17,10 @@ const options = {
   closeOnClick: false,
   pauseOnHover: false
 }
+
+Vue.use(VueGtag, {
+  config: { id: "G-7V3PX5TM85" }
+}, router);
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
