516bd8045bccf1c0ccc150807bcc57246ca25da8
Anant Jain <anant.j2409@gmail.com>
2021-06-24 16:25:47 -0400

    Adding vuelidate

--- project/src/components/JoinSessionPage.vue
+++ project/src/components/JoinSessionPage.vue
@@ -1,54 +1,136 @@
 <template>
-<div class="vertical-center">
-  <div class="container h-100">
-  <div class="row h-100 justify-content-center align-items-center">
-    <form class="col-12" novalidate>
-      <div class="form-group">
-        <label for="usernameInput">Username</label>
-        <input type="text" class="form-control" id="username" placeholder="Enter a username">
+  <div class="vertical-center">
+    <div class="container h-100">
+      <div class="row h-100 justify-content-center align-items-center">
+ <b-form @submit.stop.prevent="onSubmit">
+      <b-form-group id="username-label" label="Username *" label-for="username">
+        <b-form-input
+          id="username"
+          name="username"
+          placeholder="Enter Username"
+          v-model="$v.form.username.$model"
+          :state="validateState('username')"
+          aria-describedby="username-feedback"
+        ></b-form-input>
+
+    <b-form-valid-feedback
+          id="username-feedback"
+        >Looks good</b-form-valid-feedback>
+        <b-form-invalid-feedback
+          id="username-feedback"
+        >This is a required field and must be at least 3 characters.</b-form-invalid-feedback>
+      </b-form-group>
+
+
+      <b-button class="ml-2" variant="danger" @click="resetForm()">Back</b-button>
+      <b-button class="ml-2" variant="warning" @click="resetForm()">Reset</b-button>
+      <b-button type="submit" variant="success">Submit</b-button>
+    </b-form>
+        <!-- <form class="col-12" @submit.prevent="handleSubmit">
+          <div class="form-group">
+            <label for="username">First Name</label>
+            <input
+              type="text"
+              v-model="user.username"
+              id="username"
+              name="username"
+              class="form-control"
+              :class="{ 'is-invalid': submitted && $v.user.username.$error }"
+            />
+            <div
+              v-if="submitted && !$v.user.username.required"
+              class="invalid-feedback"
+            >
+              Username is required
+            </div>
+          </div>
+          <div class="text-center">
+            <button
+              type="button"
+              class="btn btn-lg btn-joinSession btn-danger"
+            >
+              Back
+            </button>
+            <button
+              type="button"
+              class="btn btn-lg btn-joinSession btn-success"
+            >
+              Submit
+            </button>
+          </div>
+        </form> -->
       </div>
-      <div class="form-group">
-        <label for="sessionIdInput">Session Id</label>
-        <input type="text" class="form-control" id="sessionIdInput" placeholder="Session ID">
-      </div>
-      <div class="text-center">
-        <button type="button" class="btn btn-lg btn-joinSession btn-danger"
-            @click="toHomePage()">Back</button>
-        <button type="button" class="btn btn-lg btn-joinSession btn-success"
-            @click="validate1()">Submit</button>
-      </div>
-    </form>   
+    </div>
   </div>
-</div>
-</div>
 </template>
 
 <script>
 // import Loader from "@/components/Loader.vue";
 import store from "@/store/index.js";
+import { required, minLength } from "vuelidate/lib/validators";
 
 export default {
   name: "JoinSessionPage",
   store,
-};
+data() {
+    return {
+      form: {
+        username: null,
+        sessionId: null
+      }
+    };
+  },
+  validations: {
+    form: {
+      sessionId: {
+        required
+      },
+      username: {
+        required,
+        minLength: minLength(3)
+      }
+    }
+  },
+  methods: {
+    validateState(username) {
+      const { $dirty, $error } = this.$v.form[username];
+      return $dirty ? !$error : null;
+    },
+    resetForm() {
+      this.form = {
+        username: null,
+        sessionId: null
+      };
+
+      this.$nextTick(() => {
+        this.$v.$reset();
+      });
+    },
+    onSubmit() {
+      this.$v.form.$touch();
+      if (this.$v.form.$anyError) {
+        return;
+      }
+
+      alert("Form submitted!");
+    }
+  }
+  }
 </script>
 
 <style scoped>
-.vertical-center{
-  height: 70vh; 
+.vertical-center {
+  height: 70vh;
   /* text-align: center; */
   align-items: center;
 }
 .container {
-  max-width: 20vw;
+  max-width: 25vw;
 }
-input{
-  margin-bottom: 20px;
-}
-button{
+button {
   margin: 10px;
 }
-@media only screen and (max-width:600px) {
+@media only screen and (max-width: 600px) {
   .outerbtn {
     width: 90vw;
   }
@@ -56,6 +138,5 @@ button{
   .container {
     max-width: 90vw;
   }
-
 }
 </style>
--- project/src/main.js
+++ project/src/main.js
@@ -1,65 +1,79 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Toast from "vue-toastification"
-import "./assets/alert.css"
-import 'bootstrap/dist/css/bootstrap.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Toast from "vue-toastification";
+import "./assets/alert.css";
 import VueGtag from "vue-gtag";
+import Vuelidate from "vuelidate";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.mixin({
   methods: {
     /**
-     * @param  {string} message : Content of the alert 
+     * @param  {string} message : Content of the alert
      * @param  {string} type="s" Type: success, warning, error, info, default
      * @param  {number} timeout=false : Add int in miliseconds, false for persistent
      * @param  {boolean} icon=true : Show icon/symbol
      */
     showAlert(message, type = "s", timeout = false, icon = true) {
-      this.$toast(message.toString(), { type: getType(type), timeout: parseInt(timeout), icon: icon});
+      this.$toast(message.toString(), {
+        type: getType(type),
+        timeout: parseInt(timeout),
+        icon: icon,
+      });
     },
-  }
-})
+  },
+});
 
 function getType(types) {
   types = types.toLowerCase().trim();
   switch (types) {
     case "success":
     case "s":
-      return "success"
+      return "success";
     case "error":
     case "e":
-      return "error"
+      return "error";
     case "default":
     case "d":
-      return "default"
+      return "default";
     case "info":
     case "i":
-      return "info"
+      return "info";
     case "warning":
     case "w":
-      return "warning"
+      return "warning";
     default:
-      return "success"
+      return "success";
   }
 }
 const options = {
-  position: 'top-center',
+  position: "top-center",
   maxToasts: 3,
   closeOnClick: false,
-  pauseOnHover: false
-}
+  pauseOnHover: false,
+};
 
-Vue.use(VueGtag, {
-  config: { id: "G-7V3PX5TM85" }
-}, router);
+Vue.use(
+  VueGtag,
+  {
+    config: { id: "G-7V3PX5TM85" },
+  },
+  router
+);
 
 Vue.use(Toast, options);
+Vue.use(Vuelidate);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
