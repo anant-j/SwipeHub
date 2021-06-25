278bc5f1d7dcdb17211581ab784dc6d03b3396f9
vermav6 <35265457+vermav6@users.noreply.github.com>
2021-06-25 19:45:37 -0400

    adding categories and watchers to createSession

--- project/src/components/CreateSessionPage.vue
+++ project/src/components/CreateSessionPage.vue
@@ -3,80 +3,110 @@
     <div class="container h-100">
       <div class="row h-100 justify-content-center align-items-center">
         <b-form @submit.stop.prevent="onSubmit">
-          <b-form-group
-            id="username-label"
-            label="Username *"
-            label-for="username"
-            v-if="localState >= 0"
-          >
-            <b-form-input
-              id="username"
-              name="username"
-              placeholder="Enter Username"
-              v-model="$v.form.username.$model"
-              :state="validateState('username')"
-              aria-describedby="username-feedback"
-              maxlength="30"
-            ></b-form-input>
+          <b-row>
+            <b-col>
+              <b-form-group
+                id="username-label"
+                label="Username *"
+                label-for="username"
+                v-if="localState >= 0"
+              >
+                <b-form-input
+                  id="username"
+                  name="username"
+                  placeholder="Enter Username"
+                  v-model="$v.form.username.$model"
+                  :state="validateState('username')"
+                  aria-describedby="username-feedback"
+                  maxlength="30"
+                ></b-form-input>
 
-            <b-form-valid-feedback id="username-feedback"
-              >Looks good</b-form-valid-feedback
-            >
-            <b-form-invalid-feedback id="username-feedback"
-              >User ID is required and cannot conatin special
-              characters.</b-form-invalid-feedback
-            >
-          </b-form-group>
-          <br />
-
-          <div v-if="localState >= 1">
-            <label class="typo__label">Country</label>
-            <multiselect
-              v-model="country"
-              :options="countryOptions"
-              :custom-label="countryName"
-              placeholder="Select one"
-              :allow-empty="false"
-              label="name"
-              :searchable="true"
-              track-by="name"
-              deselectLabel = "Currently Selected"
-            ></multiselect>
-            <br />
-          </div>
+                <b-form-valid-feedback id="username-feedback"
+                  >Looks good</b-form-valid-feedback
+                >
+                <b-form-invalid-feedback id="username-feedback"
+                  >User ID is required and cannot conatin special
+                  characters.</b-form-invalid-feedback
+                >
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <div v-if="localState >= 0">
+                <label class="typo__label">Country</label>
+                <multiselect
+                  v-model="country"
+                  :options="countryOptions"
+                  placeholder="Select one"
+                  :allow-empty="false"
+                  label="name"
+                  :searchable="true"
+                  track-by="name"
+                  deselectLabel="Currently Selected"
+                ></multiselect>
+              </div>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <div v-if="localState >= 1">
+                <label class="typo__label">Language</label>
+                <multiselect
+                  v-model="language"
+                  :options="languageOptions"
+                  placeholder="Select one"
+                  :allow-empty="false"
+                  label="name"
+                  :searchable="true"
+                  track-by="name"
+                  deselectLabel="Currently Selected"
+                ></multiselect>
+                <br />
+              </div>
+            </b-col>
+            <b-col>
+              <div v-if="localState >= 1">
+                <label class="typo__label">Platform</label>
+                <multiselect
+                  v-model="platform"
+                  :options="included(this.country)"
+                  placeholder="Select one"
+                  :allow-empty="false"
+                  label="name"
+                  :searchable="true"
+                  track-by="name"
+                  deselectLabel="Currently Selected"
+                >
+                  <template slot="singleLabel"
+                    ><img
+                      class="option__image"
+                      height="20px"
+                      :src="platform.logo"
+                      :alt="platform.name"
+                    /><span class="option__desc"
+                      ><span class="option__title">
+                        {{ platform.name }}</span
+                      ></span
+                    ></template
+                  >
+                </multiselect>
+                <br />
+              </div>
+            </b-col>
+          </b-row>
 
           <div v-if="localState >= 2">
-            <label class="typo__label">Language</label>
+            <label class="typo__label">Categories</label>
             <multiselect
-              v-model="language"
-              :options="languageOptions"
-              :custom-label="langName"
-              placeholder="Select one"
-              :allow-empty="false"
+              v-model="category"
+              placeholder="Search or add a Category"
               label="name"
-              :searchable="true"
               track-by="name"
-              deselectLabel = "Currently Selected"
-            ></multiselect>
-            <br />
-          </div>
-
-          <div v-if="localState >= 3">
-            <label class="typo__label">Platform</label>
-            <multiselect
-              v-model="platform"
-              :options="included(this.country)"
-              :custom-label="platformName"
-              placeholder="Select one"
-              :allow-empty="false"
-              label="name"
-              :searchable="true"
-              track-by="name"
-              deselectLabel = "Currently Selected"
+              :options="categoryOptions"
+              :multiple="true"
+              :taggable="true"
+              @tag="addTag"
             >
-            <template slot="singleLabel"><img class="option__image" height="30px" :src="platform.logo" :alt="platform.name"><span class="option__desc"><span class="option__title">  {{ platform.name }}</span></span></template>
             </multiselect>
-            <br />
           </div>
 
           <br />
@@ -130,6 +160,8 @@ export default {
       platformOptions: data.platforms,
       country: data.defaultCountry,
       countryOptions: data.countries,
+      category: null,
+      categoryOptions: data.categories,
     };
   },
   validations: {
@@ -140,6 +172,20 @@ export default {
       },
     },
   },
+  watch: {
+    localState(value) {
+      if (value >= 2) {
+        this.submitButton = "Submit";
+      } else {
+        this.submitButton = "Next";
+      }
+    },
+    "form.username": function (value) {
+      console.log(value);
+      this.form.username = this.form.username.trim();
+      //to work with changes in prop
+    },
+  },
   methods: {
     included(country) {
       let tempPlatformOptions = [];
@@ -148,18 +194,10 @@ export default {
           tempPlatformOptions.push(this.platformOptions[index]);
         }
       }
-      if (!tempPlatformOptions.includes(this.platform)){
-      this.platform = tempPlatformOptions[0];}
+      if (!tempPlatformOptions.includes(this.platform)) {
+        this.platform = tempPlatformOptions[0];
+      }
       return tempPlatformOptions;
-    },
-    langName({ name }) {
-      return `${name}`;
-    },
-    platformName({ name }) {
-      return `${name}`;
-    },
-    countryName({ name }) {
-      return `${name}`;
     },
     validateState(state) {
       const { $dirty, $error } = this.$v.form[state];
@@ -170,23 +208,21 @@ export default {
         username: null,
       };
       (this.localState = 0),
-        (this.submitButton = "Next"),
         (this.language = data.defaultLanguage),
         (this.platform = data.defaultPlatform),
-        (this.country = data.defaultCountry);
+        (this.country = data.defaultCountry),
+        (this.category = null);
       this.$nextTick(() => {
         this.$v.$reset();
       });
     },
     nextPage() {
-      if(this.form.username == null) {
-        this.$v.form.$touch();
+      this.$v.form.$touch();
+      if (this.$v.form.$anyError) {
+        return;
       }
-      if (this.form.username != null && this.localState < 3) {
+      if (this.form.username != null && this.localState < 4) {
         this.localState += 1;
-      }
-      if (this.localState == 3) {
-        this.submitButton = "Submit";
       }
     },
     onSubmit() {
@@ -194,19 +230,27 @@ export default {
       if (this.$v.form.$anyError) {
         return;
       }
-      if (this.localState < 3){
+      if (this.localState < 4) {
         this.localState += 1;
         return;
       }
       alert("Form submitted!");
     },
   },
+  addTag(newTag) {
+    const tag = {
+      name: newTag,
+      code: newTag.substring(0, 2) + Math.floor(Math.random() * 10000000),
+    };
+    this.categoryOptions.push(tag);
+    this.category.push(tag);
+  },
 };
 </script>
 
 <style scoped>
 .vertical-center {
-  height: 70vh;
+  height: 85vh;
   /* text-align: center; */
   align-items: center;
 }
@@ -217,7 +261,7 @@ export default {
 }
 
 .container {
-  max-width: 25vw;
+  max-width: 45vw;
 }
 button {
   margin: 10px;
