b50ec238e6b207ca478bed43240d9c3f9c1dd47c
vermav6 <35265457+vermav6@users.noreply.github.com>
2021-07-07 22:49:39 -0400

    seach bar in match page

--- project/src/views/Matches.vue
+++ project/src/views/Matches.vue
@@ -16,34 +16,41 @@
         >
       </div>
     </div>
-    <div
-      id="cardHolder"
-      class="row row-cols-1 row-cols-sm-3 row-cols-md-4 g-3 mt-3 mb-3"
-      @click="pageActivity()"
-    >
+    <div>
+      <input
+        v-model="searchField"
+        placeholder="Enter Search Field"
+        style="width: 90%; margin: 10px; padding: 5px"
+      />
       <div
-        class="col"
-        v-for="item in this.$store.state.matchData"
-        :key="item.movieId"
+        id="cardHolder"
+        class="row row-cols-1 row-cols-sm-3 row-cols-md-4 g-3 mt-3 mb-3"
+        @click="pageActivity()"
       >
-        <div class="card text-white bg-dark h-100 text-center">
-          <img
-            class="card-img-top"
-            style="max-height: 50vh; object-fit: contain; margin-top: 30px"
-            alt="..."
-            v-lazy="{
-              src: item.posterURL,
-              loading: 'https://i.giphy.com/media/N256GFy1u6M6Y/giphy.webp',
-            }"
-          />
-          <div class="card-body">
-            <h5 class="card-title">
-              <b>{{ item.title }}</b>
-            </h5>
-            <p class="card-text">{{ item.description }}</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Released on {{ item.release }}</small>
+        <div
+          class="col"
+          v-for="item in this.localMatchStore"
+          :key="item.movieId"
+        >
+          <div class="card text-white bg-dark h-100 text-center">
+            <img
+              class="card-img-top"
+              style="max-height: 50vh; object-fit: contain; margin-top: 30px"
+              alt="..."
+              v-lazy="{
+                src: item.posterURL,
+                loading: 'https://i.giphy.com/media/N256GFy1u6M6Y/giphy.webp',
+              }"
+            />
+            <div class="card-body">
+              <h5 class="card-title">
+                <b>{{ item.title }}</b>
+              </h5>
+              <p class="card-text">{{ item.description }}</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Released on {{ item.release }}</small>
+            </div>
           </div>
         </div>
       </div>
@@ -61,6 +68,8 @@ export default {
   data: () => ({
     timer: null,
     lastInteraction: new Date(),
+    localMatchStore: [],
+    searchField: "",
   }),
   created() {
     window.addEventListener("scroll", this.pageActivity);
@@ -77,10 +86,31 @@ export default {
     this.$store.state.loader = true;
     this.$store.state.activePage = 2;
     this.matchPoll();
+    this.localMatchStore = this.$store.state.matchData;
   },
   destroyed() {
     window.removeEventListener("scroll", this.pageActivity);
     clearTimeout(this.timer);
+  },
+  watch: {
+    searchField(value) {
+      if (value == "") {
+        this.localMatchStore = this.$store.state.matchData;
+      } else {
+        // searchAlgorithm(value)
+        this.localMatchStore = [];
+        const searchValue = value.toLowerCase();
+        const localMovieData = this.$store.state.matchData;
+        for (const movie of localMovieData) {
+          if (
+            movie.title.toLowerCase().includes(searchValue) ||
+            movie.description.toLowerCase().includes(searchValue) ||
+            movie.release.toLowerCase().includes(searchValue)
+          )
+            this.localMatchStore.push(movie);
+        }
+      }
+    },
   },
   methods: {
     pageActivity() {
@@ -143,6 +173,7 @@ export default {
             this.updateUsersJoinLeaveNotification(Object.keys(userData));
             this.$store.state.usersData = userDataArray;
             this.$store.state.matchData = movieList;
+            this.localMatchData = movieList;
             this.$store.state.totalMatches = movieList.length;
           })
           .catch(() => {
