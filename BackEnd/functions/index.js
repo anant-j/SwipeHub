aa97a39eda24972051795bb3391648ddbe200fd2
Anant Jain <anant.j2409@gmail.com>
2021-08-04 21:54:04 -0400

    Subsequent Cards + Adding AppCheck

--- BackEnd/functions/index.js
+++ BackEnd/functions/index.js
@@ -10,6 +10,11 @@ const expectedToken = TelegramToken.split(":")[1].toLowerCase();
 const sessionDb = admin.database();
 
 exports.registerTenant = functions.https.onCall(async (data, context) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError(
+        "failed-precondition",
+        "The function must be called from an App Check verified app.");
+  }
   if (data.requestType === "join") {
     const username = data.username;
     const sessionId = data.sessionId;
@@ -82,99 +87,49 @@ exports.swipe = functions.database.ref("{sessionId}/users/{userId}/swipes")
       const sessionId = context.params.sessionId;
       const userId = context.params.userId;
       const mySwipes = change.after.val();
-      const snap = await sessionDb.ref(sessionId).child("users").once("value");
-      const matches = getMatches(snap.val());
-      sessionDb.ref(sessionId).update({
-        [`sessionActivity/users/${userId}/swipes`]: Object.keys(mySwipes).length,
-        "sessionActivity/matches": matches,
-      });
-    });
-
-/**
- * @param {any} data
- * @param {any} userId
- * @param {any} movieId
- * @param {any} updateVariable
- * @return {any}
- */
-function getMatches(data) {
-  const matches = [];
-  const allLikes = [];
-  const matchMap = {};
-  if (Object.keys(data).length <=1) {
-    return matches;
-  }
-  for (const eachUser of Object.keys(data)) {
-    const likes = data[eachUser]["swipes"];
-    for (const movieId in likes) {
-      if (likes[movieId]) {
-        allLikes.push(movieId);
+      const snap = await sessionDb.ref(sessionId).once("value");
+      const matches = getMatches(snap.val()["users"]);
+      const mySwipesLength = Object.keys(mySwipes).length;
+      const mediaOrder = snap.val()["sessionActivity"]["mediaOrder"];
+      const mediaOrderLength = mediaOrder.length;
+      if (mediaOrderLength >= 300) {
+        if (!(mediaOrder.includes("null"))) {
+          mediaOrder.push("null");
+        }
+      } else if ((mediaOrderLength - mySwipesLength == 9) && (!(mediaOrder[mediaOrderLength - 1] == "null"))) {
+        const page = getPageNumber(mediaOrderLength);
+        const sessionInfo = snap.val()["sessionInfo"];
+        const newData = await mediaData(sessionInfo, page);
+        for (const mediaId of newData) {
+          if (!(mediaOrder.includes(mediaId))) {
+            mediaOrder.push(mediaId);
+          }
+        }
       }
-    }
-  }
-  for (const mediaId of allLikes) {
-    if (!matchMap[mediaId]) {
-      matchMap[mediaId] = 1;
-    } else {
-      matchMap[mediaId] += 1;
-    }
-    if (matchMap[mediaId] == Object.keys(data).length && Object.keys(data).length > 1) {
-      matches.push(mediaId);
-    }
-  }
-  return matches;
-}
+      sessionDb.ref(sessionId).update({
+        [`sessionActivity/users/${userId}/swipes`]: mySwipesLength,
+        "sessionActivity/matches": matches,
+        "sessionActivity/mediaOrder": mediaOrder,
+      });
+      return;
+    });
 
 exports.generateInitialData = functions.database.ref("{sessionId}")
     .onCreate(async (snapshot, context) => {
       const sessionInfo = snapshot.val().sessionInfo;
       const sessionId = snapshot.key;
-      const categories = sessionInfo.categories;
-      const languages = sessionInfo.languages;
-      const platform = sessionInfo.platform;
-      const region = sessionInfo.region;
-      let sortby = sessionInfo.order;
-      let dataSet = [];
-      const movie = sessionInfo.isMovie;
-      if (movie === true) {
-        if (sortby == "Popularity") {
-          sortby = "popularity.desc";
-        } else if (sortby == "Release") {
-          sortby = "primary_release_date.desc";
-        } else if (sortby == "Revenue") {
-          sortby = "revenue.desc";
-        }
-        dataSet = await generateMovieList(
-            languages,
-            categories,
-            platform,
-            region,
-            sortby,
-            1,
-        );
-      } else {
-        if (sortby == "Popularity") {
-          sortby = "popularity.desc";
-        } else if (sortby == "Release") {
-          sortby = "first_air_date.desc";
-        } else if (sortby == "Revenue") {
-          sortby = "popularity.desc";
-        }
-        dataSet = await generateTVList(
-            languages,
-            categories,
-            platform,
-            region,
-            sortby,
-            1,
-        );
-      }
+      const dataSet = await mediaData(sessionInfo, 1);
       return sessionDb.ref(sessionId).child("sessionActivity").update({
         mediaOrder: dataSet,
       });
     });
 
 exports.leaveSession = functions.https.onCall(async (data, context) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError(
+        "failed-precondition",
+        "The function must be called from an App Check verified app.");
+  }
   const userId = context.auth.token.userId;
   const sessionId = context.auth.token.sessionId;
   const isCreator = context.auth.token.isCreator;
@@ -194,84 +149,8 @@ exports.leaveSession = functions.https.onCall(async (data, context) => {
   sessionDb.ref(sessionId).child("sessionActivity").update({
     matches: matches,
   });
+  return;
 });
-
-// exports.subsequentCards = functions.https.onRequest(async (req, res) => {
-//   try {
-//     res.set("Access-Control-Allow-Origin", "*");
-//     const id = req.query.id.toUpperCase();
-//     let totalCards = parseInt(req.query.totalCards);
-//     const sessionDb = admin.firestore().collection("sessions").doc(id);
-//     const doc = await sessionDb.get();
-//     if (!isValidSession(doc)) {
-//       res.status(404).send("Session doesn't exist");
-//       return;
-//     } else {
-//       const currentMovieSize = Object.keys(doc.data().mediaInfo).length - 1;
-//       const movie = doc.data().isMovie;
-//       const languages = doc.data().languages;
-//       const categories = doc.data().categories;
-//       const platform = doc.data().platform;
-//       const region = doc.data().region;
-//       const sortby = doc.data().order;
-//       let dataSet = {};
-//       const oldDataSet = doc.data().mediaInfo;
-//       totalCards = upperValue(totalCards);
-//       if (totalCards >= currentMovieSize) {
-//         const pageNum = totalCards / 20 + 1;
-//         if (movie === "true") {
-//           dataSet = await generateMovieList(
-//               languages,
-//               categories,
-//               platform,
-//               region,
-//               sortby,
-//               pageNum,
-//           );
-//         }
-//         if (movie != "true") {
-//           dataSet = await generateTVList(
-//               languages,
-//               categories,
-//               platform,
-//               region,
-//               sortby,
-//               pageNum,
-//           );
-//         }
-//         const newOrder = dataSet["order"];
-//         for (let index = 0; index < newOrder.length; index++) {
-//           const key = newOrder[index];
-//           oldDataSet["order"].push(key);
-//           oldDataSet[key] = dataSet[key];
-//         }
-//         const data = {
-//           mediaInfo: oldDataSet,
-//         };
-//         await admin
-//             .firestore()
-//             .collection("sessions")
-//             .doc(id)
-//             .set(data, {merge: true});
-//       } else {
-//         let lower = totalCards;
-//         const upper = totalCards + 19;
-//         const oldOrder = oldDataSet["order"];
-//         dataSet["order"] = [];
-//         for (lower; lower <= upper; lower++) {
-//           const key = oldOrder[lower];
-//           dataSet[key] = oldDataSet[key];
-//           dataSet["order"].push(key);
-//         }
-//       }
-//       res.status(200).send({movies: dataSet});
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     sendErrorNotification("subsequentCards", error);
-//     res.status(500).send("error");
-//   }
-// });
 
 exports.deploymessages = functions.https.onRequest(async (req, res) => {
   try {
@@ -308,6 +187,92 @@ exports.deploymessages = functions.https.onRequest(async (req, res) => {
     res.status(500).send("error");
   }
 });
+
+/**
+ * @param {any} data
+ * @param {any} userId
+ * @param {any} movieId
+ * @param {any} updateVariable
+ * @return {any}
+ */
+function getMatches(data) {
+  const matches = [];
+  const allLikes = [];
+  const matchMap = {};
+  if (Object.keys(data).length <=1) {
+    return matches;
+  }
+  for (const eachUser of Object.keys(data)) {
+    const likes = data[eachUser]["swipes"];
+    for (const movieId in likes) {
+      if (likes[movieId]) {
+        allLikes.push(movieId);
+      }
+    }
+  }
+  for (const mediaId of allLikes) {
+    if (!matchMap[mediaId]) {
+      matchMap[mediaId] = 1;
+    } else {
+      matchMap[mediaId] += 1;
+    }
+    if (matchMap[mediaId] == Object.keys(data).length && Object.keys(data).length > 1) {
+      matches.push(mediaId);
+    }
+  }
+  return matches;
+}
+
+/**
+ * @param  {string} sessionInfo
+ * @param  {number} page
+ */
+async function mediaData(sessionInfo, page) {
+  const categories = sessionInfo.categories;
+  const languages = sessionInfo.languages;
+  const platform = sessionInfo.platform;
+  const region = sessionInfo.region;
+  let sortby = sessionInfo.order;
+  const movie = sessionInfo.isMovie;
+  let dataSet = [];
+  if (movie === true) {
+    if (sortby == "Popularity") {
+      sortby = "popularity.desc";
+    } else if (sortby == "Release") {
+      sortby = "primary_release_date.desc";
+    } else if (sortby == "Revenue") {
+      sortby = "revenue.desc";
+    }
+    dataSet = await generateMovieList(
+        languages,
+        categories,
+        platform,
+        region,
+        sortby,
+        page,
+    );
+  } else {
+    if (sortby == "Popularity") {
+      sortby = "popularity.desc";
+    } else if (sortby == "Release") {
+      sortby = "first_air_date.desc";
+    } else if (sortby == "Revenue") {
+      sortby = "popularity.desc";
+    }
+    dataSet = await generateTVList(
+        languages,
+        categories,
+        platform,
+        region,
+        sortby,
+        page,
+    );
+  }
+  if (dataSet.length < 20) {
+    dataSet.push("null");
+  }
+  return dataSet;
+}
 
 /**
  * @param {string} caller
@@ -421,20 +386,20 @@ function randomSessionCode() {
   return result;
 }
 
-// /**
-//  * @param  {number} numberOfCards
-//  * @return {number} val
-//  */
-// function upperValue(numberOfCards) {
-//   const ceiledNum = Math.ceil(numberOfCards / 10);
-//   let val = 0;
-//   if (ceiledNum % 2 == 0) {
-//     val = ceiledNum * 10;
-//   } else {
-//     val = (ceiledNum + 1) * 10;
-//   }
-//   return val;
-// }
+/**
+ * @param  {number} numberOfCards
+ * @return {number} val
+ */
+function getPageNumber(numberOfCards) {
+  const ceiledNum = Math.ceil(numberOfCards / 10);
+  let val = 0;
+  if (ceiledNum % 2 == 0) {
+    val = ceiledNum;
+  } else {
+    val = (ceiledNum + 1);
+  }
+  return val;
+}
 
 /**
  * @param  {String} userId
